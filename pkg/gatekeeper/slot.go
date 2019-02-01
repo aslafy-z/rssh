feabcfe2c9ff0d5c6852922c7fc969266db01990
Germain GAU <germain.gau@gmail.com>
2019-02-01 17:04:06 +0100

    Gatekeeper POC

--- pkg/gatekeeper/slot.go
+++ pkg/gatekeeper/slot.go
@@ -1,26 +1,43 @@
 package gatekeeper
 
-import "errors"
+import (
+	"errors"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
 
 type AgentSlot struct {
-	Host    string `json:"host"`
-	Port    uint16 `json:"port"`
-	AgentID string `json:"agentID"`
+	Domain      string `json:"domain"`
+	Host        string `json:"host"`
+	Port        uint16 `json:"port"`
+	AgentID     string `json:"agentID"`
+	Established bool   `json:"established"`
 }
 
-type Gate struct {
-	Host string `json:"host"`
-	Port uint16 `json:"port"`
-}
+func (g *GateKeeper) AllocateAgentSlot(domain string) (*AgentSlot, error) {
+	for x := 0; x < len(g.clients); x++ {
+		if strings.Compare(domain, g.clients[x].Domain) == 0 {
+			log.Warn().
+				Str("domain", domain).
+				Uint16("bound_port", g.clients[x].Port).
+				Msg("Domain already registered for another agent")
+			return nil, errors.New("domain already registered")
+		}
+	}
 
-type GateKeeper struct {
-	backends []Gate
-}
+	port, err := findAvailablePort(g.lowPort, g.highPort)
+	if err != nil {
+		return nil, err
+	}
 
-func NewGateKeeper(addr string, port uint16) (*GateKeeper, error) {
-	return nil, errors.New("NotImplemented")
-}
-
-func (g *GateKeeper) AllocateAgentSlot() (*AgentSlot, error) {
-	return nil, errors.New("NotImplemented")
+	slot := &AgentSlot{
+		Domain:      domain,
+		Host:        "127.0.0.1",
+		Port:        port,
+		AgentID:     "test",
+		Established: false,
+	}
+	g.clients = append(g.clients, *slot)
+	return slot, nil
 }
